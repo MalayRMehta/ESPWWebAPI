1400ed71240a72bf29ee7f3bd027a70ccc49bf83
Your Name <cmalaymehta@gmail.com>
2025-01-25 18:36:58 +0000

    Add project files.

---
+++ DAL/DAL.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.SqlClient;
+using ESPWWebAPI.Model;
+namespace ESPWWebAPI.DAL
+{
+	public class DAL
+	{
+		public List<ClientInfo> listClients = new List<ClientInfo>();
+
+		public void OnGet()
+		{
+			try
+			{
+				String connectionString = "Data Source=.\\sqlexpress;Initial Catalog=AberInstruments;Integrated Security=True;";
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+					String sql = "SELECT * FROM clients";
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								ClientInfo clientInfo = new ClientInfo();
+								clientInfo.id = "" + reader.GetInt32(0);
+								clientInfo.name = reader.GetString(1);
+								clientInfo.doses = "" + reader.GetInt32(2);
+								clientInfo.time = "" + reader.GetDouble(3);
+								clientInfo.mass = "" + reader.GetDouble(4);
+								clientInfo.created_at = reader.GetDateTime(5).ToString();
+
+								listClients.Add(clientInfo);
+							}
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Exception " + ex.ToString());
+			}
+		}
+
+
+	}
+}
---
+++ Model/ESPWModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ESPWWebAPI.Model
+{
+	public class ClientInfo
+	{
+		public String id;
+		public String name;
+		public String doses;
+		public String time;
+		public String mass;
+		public String created_at;
+
+	}
+}
