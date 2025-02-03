ea929c55df98cf7c5712bd207fc00057e9b859dd
Your Name <cmalaymehta@gmail.com>
2025-02-03 18:38:54 +0000

    Calling URL from POST.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ESPWWebAPI.DAL;
+using ESPWWebAPI.Utility;
 namespace ESPWWebAPI.Controllers
 {
 	[ApiController]
@@ -27,11 +28,40 @@ namespace ESPWWebAPI.Controllers
 
 
 		[HttpPost]
-		public IActionResult Post(ClientInfo clientInfo)
+		public async void Post(ClientInfo clientInfo)
 		{
 			DAL.DAL objDAL = new DAL.DAL();
 			var result = objDAL.OnPost(clientInfo);
-			return Ok(result);
+			
+			string url = "http://192.168.4.1";
+
+			// Parameters to include in the POST request
+			var parameters = new Dictionary<string, string>
+		{
+			{ "PARAM_INPUT_1", "10" },
+			{ "PARAM_INPUT_2", "30" },
+			{ "PARAM_INPUT_3", "40" },
+			{ "PARAM_INPUT_4", "40" }
+		};
+
+			// Create an instance of the HttpService class
+			var httpService = new HttpService();
+
+			// Call the SendPostRequestAsync method and get the response
+			string response = await httpService.SendPostRequestAsync(url, parameters);
+
+			// Print the response (you can also handle it differently)
+			Console.WriteLine($"Response: {response}");
+
+
 		}
+
+
+
 	}
+
+
+
+
+
 }
---
+++ Utility/HttpService.cs
@@ -0,0 +1,39 @@
+﻿namespace ESPWWebAPI.Utility
+{
+	using System;
+	using System.Net.Http;
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+
+	public class HttpService
+	{
+		// Method to send a POST request
+		public async Task<string> SendPostRequestAsync(string url, Dictionary<string, string> parameters)
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				// Convert the parameters to FormUrlEncodedContent
+				var content = new FormUrlEncodedContent(parameters);
+
+				try
+				{
+					// Send POST request
+					HttpResponseMessage response = await client.PostAsync(url, content);
+
+					// Ensure successful response
+					response.EnsureSuccessStatusCode();
+
+					// Read and return the response content
+					string responseContent = await response.Content.ReadAsStringAsync();
+					return responseContent;
+				}
+				catch (Exception ex)
+				{
+					// Handle any exceptions and return the error message
+					return $"Error: {ex.Message}";
+				}
+			}
+		}
+	}
+
+}
