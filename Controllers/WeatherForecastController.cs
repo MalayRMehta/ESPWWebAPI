752aec2261a0d4d730dc3066f2e4de1e5bf7ecf0
Your Name <cmalaymehta@gmail.com>
2025-02-08 20:02:12 +0000

    Call to ESP-32.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -5,56 +5,96 @@ namespace ESPWWebAPI.Controllers
 {
 	[ApiController]
 	[Route("[controller]")]
-	public class WeatherForecastController : ControllerBase
+	public class ESPWController : ControllerBase
 	{
 
 
 
 
-		private readonly ILogger<WeatherForecastController> _logger;
+		private readonly ILogger<ESPWController> _logger;
 
-		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		public ESPWController(ILogger<ESPWController> logger)
 		{
 			_logger = logger;
 		}
 
-		[HttpGet]
-		public IActionResult Get()
-		{
-			DAL.DAL objDAL = new DAL.DAL();
-			var result = objDAL.OnGet();			
-			return Ok(result);
-		}
+		 [HttpGet]
+		 public IActionResult Get()
+		 {
+		 	DAL.DAL objDAL = new DAL.DAL();
+		 	var result = objDAL.OnGet();			
+		 	return Ok(result);
+		 }
+
 
 
 		[HttpPost]
 		public async void Post(ClientInfo clientInfo)
 		{
-			DAL.DAL objDAL = new DAL.DAL();
-			var result = objDAL.OnPost(clientInfo);
-			
+
 			string url = "http://192.168.4.1";
 
 			// Parameters to include in the POST request
 			var parameters = new Dictionary<string, string>
-		{
-			{ "PARAM_INPUT_1", "10" },
-			{ "PARAM_INPUT_2", "30" },
-			{ "PARAM_INPUT_3", "40" },
-			{ "PARAM_INPUT_4", "40" }
-		};
+	 	{
+	 		{ "input1", "1" },
+	 		{ "input2", "1" },
+	 		{ "input3", "2"},
+	 		{ "input4", "4"}
+	 	};
 
-			// Create an instance of the HttpService class
+			// 		// Create an instance of the HttpService class
 			var httpService = new HttpService();
 
-			// Call the SendPostRequestAsync method and get the response
+			// 		// Call the SendPostRequestAsync method and get the response
 			string response = await httpService.SendPostRequestAsync(url, parameters);
+			Console.WriteLine("TEsting: ");
 
-			// Print the response (you can also handle it differently)
+			// 		// Print the response (you can also handle it differently)
 			Console.WriteLine($"Response: {response}");
-
+			//return Ok(result);
 
 		}
+
+
+
+		[HttpGet]
+	 	public async void GetESPW(string input1)
+	 	{
+
+			string url = "http://192.168.4.1";
+
+	 		// Parameters to include in the POST request
+	 		var parameters = new Dictionary<string, string>
+	 	{
+	 		{ "input1", input1 },
+	 		{ "input2", "1" },
+	 		{ "input3", "2"},
+	 		{ "input4", "4"}
+	 	};
+
+	// 		// Create an instance of the HttpService class
+	 		var httpService = new HttpService();
+
+	// 		// Call the SendPostRequestAsync method and get the response
+	 		string response = await httpService.SendPostRequestAsync(url, parameters);
+	 		Console.WriteLine("TEsting: ");
+
+	// 		// Print the response (you can also handle it differently)
+	 		Console.WriteLine($"Response: {response}");
+ 	     	//return Ok(result);
+
+	 	}
+
+		// [HttpPost]
+		// public async void Post(ClientInfo clientInfo)
+		// {
+		// 	DAL.DAL objDAL = new DAL.DAL();
+		// 	var result = objDAL.OnPost(clientInfo);
+			
+			
+
+		// }
 
 
 
