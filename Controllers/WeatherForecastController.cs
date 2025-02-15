0449dfba4ec324cae175405e8d55fda229aaaf1a
Your Name <cmalaymehta@gmail.com>
2025-02-15 20:26:46 +0000

    Code Cleanup

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -7,10 +7,6 @@ namespace ESPWWebAPI.Controllers
 	[Route("[controller]")]
 	public class ESPWController : ControllerBase
 	{
-
-
-
-
 		private readonly ILogger<ESPWController> _logger;
 
 		public ESPWController(ILogger<ESPWController> logger)
@@ -30,83 +26,23 @@ namespace ESPWWebAPI.Controllers
 
 		[HttpPost]
 		public async void Post(ClientInfo clientInfo)
-		{
-
-			string url = "http://192.168.4.1/get";
-
-			// Parameters to include in the POST request
-			var parameters = new Dictionary<string, string>
-	 	{
-	 		{ "input1", clientInfo.doses.ToString() },
-	 		{ "input2", clientInfo.mass.ToString() },
-	 		{ "input3", clientInfo.time.ToString()},
-	 		{ "input4", "5"}
-	 	};
-
-
-		
-
+		{		
+			//passing parameters in query string
 			string apiUrl = "http://192.168.4.1/get?input1=" + clientInfo.doses.ToString()  + "&input2="+ clientInfo.mass.ToString() + "&input3="+ clientInfo.time.ToString() + "&input4=1";
 
 			using (HttpClient client = new HttpClient())
 			{
-				// Optional: Add headers if needed (e.g., authorization)
-				// client.DefaultRequestHeaders.Add("Authorization", "Bearer your_token");
-
 				HttpResponseMessage response = await client.GetAsync(apiUrl);
 
 				if (response.IsSuccessStatusCode)
 				{
 					string responseContent = await response.Content.ReadAsStringAsync();
-
-
-
-
 				}
 
 			}
 		}
 
-					[HttpGet]
-	 	public async void GetESPW(string input1)
-	 	{
-
-			string url = "http://192.168.4.1";
-
-	 		// Parameters to include in the POST request
-	 		var parameters = new Dictionary<string, string>
-	 	{
-	 		{ "input1", input1 },
-	 		{ "input2", "1" },
-	 		{ "input3", "2"},
-	 		{ "input4", "4"}
-	 	};
-
-	// 		// Create an instance of the HttpService class
-	 		var httpService = new HttpService();
-
-	// 		// Call the SendPostRequestAsync method and get the response
-	 		string response = await httpService.SendPostRequestAsync(url, parameters);
-	 		Console.WriteLine("TEsting: ");
-
-	// 		// Print the response (you can also handle it differently)
-	 		Console.WriteLine($"Response: {response}");
- 	     	//return Ok(result);
-
-	 	}
-
-		// [HttpPost]
-		// public async void Post(ClientInfo clientInfo)
-		// {
-		// 	DAL.DAL objDAL = new DAL.DAL();
-		// 	var result = objDAL.OnPost(clientInfo);
-			
-			
-
-		// }
-
-
-
+		
 	}
 
 
